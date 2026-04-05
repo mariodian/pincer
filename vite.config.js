6a9e5ee50dbf68ddc75027bcc9d6181bb74fde5d
Mario Dian <mariodian@gmail.com>
2026-04-06 01:28:57 +0800

    fix: add typecheck to build script and set chunk size warning limit in Vite config

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig({
   build: {
     outDir: "../../dist",
     emptyOutDir: true,
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/mainview/index.html"),
