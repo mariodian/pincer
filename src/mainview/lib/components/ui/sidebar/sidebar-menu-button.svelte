516b651f59c9154bd7633bad4b093657336fb09f
Mario Dian <mariodian@gmail.com>
2026-04-07 23:41:53 +0800

    fix: update tooltip visibility logic to simplify conditions

--- src/mainview/lib/components/ui/sidebar/sidebar-menu-button.svelte
+++ src/mainview/lib/components/ui/sidebar/sidebar-menu-button.svelte
@@ -89,7 +89,7 @@
 		<Tooltip.Content
 			side="right"
 			align="center"
-			hidden={sidebar.state !== "collapsed" || sidebar.isMobile}
+			hidden={sidebar.state !== "collapsed"}
 			{...tooltipContentProps}
 		>
 			{#if typeof tooltipContent === "string"}
