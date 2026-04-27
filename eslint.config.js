cf476c59a8bcc8764552335b19329e811de72737
Mario Dian <mariodian@gmail.com>
2026-04-28 03:35:13 +0800

    chore(eslint): add ESLint configuration and scripts

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import svelte from "eslint-plugin-svelte";
+import unusedImports from "eslint-plugin-unused-imports";
+import tseslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...svelte.configs.recommended,
+  {
+    plugins: { "unused-imports": unusedImports },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
